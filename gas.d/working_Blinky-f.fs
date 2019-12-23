4802122d5ffbd828fd538cb462dad87161788e5a
wa1tnr <wa1tnr@users.noreply.github.com>
2019-12-23 15:17:40 +0000

    very good turnkey code 23 dec

---
+++ gas.d/working_Blinky-f.fs
@@ -0,0 +1,42 @@
+COLD 
+
+: RCC 40023800 ; : RCC_AHB1ENR RCC 30 + ;
+: GPIOC 40020800 ;
+: SETUPLED RCC @
+  4 OR RCC_AHB1ENR ! ( GPIOCEN )
+  4 GPIOC ! 0 GPIOC 14 + ! ;
+: LED GPIOC 14 + 2 ;
+: ON SWAP ! ;
+: OFF DROP 0 SWAP ! ;
+: DELAY DEPTH 1 - 0<
+  IF EXIT THEN
+  FOR 3 FOR 11 FOR 100
+      FOR 1 DROP NEXT
+  NEXT NEXT NEXT ;
+: BDELAY 3 DELAY ;
+: BDKDEL 8 DELAY ;
+: LDELAY 188 DELAY ;
+: FINISHMSG ."  done." ;
+( 100 blinks per minute )
+: BLINKS DEPTH 1 - 0<
+  IF EXIT THEN
+  1 - ( normalize )
+  FOR LED ON BDELAY LED OFF BDKDEL
+  NEXT FINISHMSG ;
+: LINIT  FFFFFF9D SETUPLED 3 BLINKS ;
+: RAMB          0 ; ( base of Forth vmem? )
+: FLASHB  8000000 ; ( base of flash - turnkey here )
+: MEMB   20000000 ; ( base of RAM )
+: DDP OVER OVER DUMP ( 8000000 32 -- 8000000 32 )
+  OVER OVER ( 8m 32 -- 8m 32 8m 32 )
+  + ( 8m 32 8m 32 -- 8m 32 8m32 )
+  ROT ( 8m 32 8m32 -- 32 8m32 8m )
+  DROP SWAP ( 32 8m32 8m -- 8M32 32 )
+  ;
+
+ ( - - - - - )
+
+: DOME FOR 1 DROP NEXT 2B EMIT 2B EMIT 2B EMIT 20 EMIT ;
+
+( newline 50 char 10 - delays in ms in minicom used 23 December )
+( control A T accesses the Terminal settings in minicom )
