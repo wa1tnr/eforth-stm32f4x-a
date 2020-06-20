c8be396a943539f17ba6737faed83eb5059f338d
wa1tnr <wa1tnr@users.noreply.github.com>
2020-06-20 08:18:01 +0000

    Seems fine as is.
    A real commit, though not well-tested.
    
    	modified:   rebuild-dd.fs
    
    On branch assy-exp-ae-console-

--- lumex_displ.d/0-eforth_code.d/rebuild-dd.fs
+++ lumex_displ.d/0-eforth_code.d/rebuild-dd.fs
@@ -1,4 +1,4 @@
-\ Sat Jun 20 08:00:00 UTC 2020
+\ Sat Jun 20 08:10:45 UTC 2020
 
 \ remove some PORTC stuff in prep for USART6.
 
@@ -160,35 +160,13 @@
   ;
 
 : vers space
-  ." 0.0.0.1.u- "
-  ." Sat Jun 20 08:00:00 UTC 2020 "
-;
+  ." 0.0.0.1.v- "
+  ." Sat Jun 20 08:10:45 UTC 2020 "
+; \ green orange red blue
+ ( trial run: ) ( LINIT ) ( green OUTPUT )
+ ( green on ) ( green off )
+ ( 5 blinks ) ( blue on LDELAY blue off )
 
-.( 0 ERASE_SECTOR )
-
-\ green orange red blue
- ( trial run: )
- ( LINIT )
- ( green OUTPUT )
- ( green on )
- ( green off )
- ( orange OUTPUT )
- ( orange on )
- ( orange off )
- ( red OUTPUT )
- ( red on )
- ( red off )
-
- ( here's how to setup any port pin on PORTC )
- (  as OUTPUT: )
- ( 1 4 << ok ) ( 1 is just the universal binary bit )
- ( 4 here is the selected port pin - PORTC MODER2 - low bit )
- ( as in 8.4.1 )
- ( GPIOC_MODER @ SWAP OR GPIOC_MODER ! ok )
- ( that was PORTC_2  aka  PORTC MODER2 )
- ( PORTC_1 is the D13 LED )
- ( here's PORTC_3 aka PORTC MODER3 )
- ( 1 6 << ok )
- ( GPIOC_MODER @ SWAP OR GPIOC_MODER ! ok )
+.( 0 ERASE_SECTOR ) ( TURNKEY )
 
  ( - - - - - )
