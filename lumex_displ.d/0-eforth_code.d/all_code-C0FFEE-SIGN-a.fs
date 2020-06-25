a832a3bfec71bd45df76560ea9a9e2f8334e87da
wa1tnr <wa1tnr@users.noreply.github.com>
2020-06-25 06:13:33 +0000

    oops - added unintentionally - okay, tho.

--- lumex_displ.d/0-eforth_code.d/all_code-C0FFEE-SIGN-a.fs
+++ lumex_displ.d/0-eforth_code.d/all_code-C0FFEE-SIGN-a.fs
@@ -1,4 +1,9 @@
 \ : c0ffee-sign
+
+\ accidental add in git. ;) 25 Jun 06:12z
+\ can stand as-is but may or may not be
+\ conflicting with the other all_code-* files.
+
 : line_end 0A outc ;
 : s_atd1=()
  \ atd1=()
@@ -7,7 +12,9 @@
  line_end
 ;
 
-: atef=(1)
+\ ref lumex-64x32_RGB_LED_command_list_V17_dot_E_03152019.pdf
+
+: atef=(1) \ default color, here '1' is a color range 0-255
  29 31 28 3D   66 65 74 61
  outc outc outc outc outc outc outc outc
  line_end
