f052e792c11f83efdd5956a5da5e23ff4c725c71
wa1tnr <wa1tnr@users.noreply.github.com>
2020-07-02 19:31:20 +0000

    Uncommited edits prior to renaming. ;)
    	modified:   _this_code-at80-aa.fs
    
    On branch console-aa-

--- lumex_displ.d/0-eforth_code.d/_this_code-at80-aa.fs
+++ lumex_displ.d/0-eforth_code.d/_this_code-at80-aa.fs
@@ -4,24 +4,62 @@
 
 \ c0ffee-sign derivatives aa
 
+: chA [ char A ] LITERAL ;
+: chB [ char B ] LITERAL ;
+: chC [ char C ] LITERAL ;
+: chD [ char D ] LITERAL ;
+: chE [ char E ] LITERAL ;
+: chF [ char F ] LITERAL ;
+: chG [ char G ] LITERAL ;
+: chH [ char H ] LITERAL ;
+: chI [ char I ] LITERAL ;
+: chJ [ char J ] LITERAL ;
+: chK [ char K ] LITERAL ;
+: chL [ char L ] LITERAL ;
+: chM [ char M ] LITERAL ;
+: chN [ char N ] LITERAL ;
+: chO [ char O ] LITERAL ;
+: chP [ char P ] LITERAL ;
+: chQ [ char Q ] LITERAL ;
+: chR [ char R ] LITERAL ;
+: chS [ char S ] LITERAL ;
+: chT [ char T ] LITERAL ;
+: chU [ char U ] LITERAL ;
+: chV [ char V ] LITERAL ;
+: chW [ char W ] LITERAL ;
+: chX [ char X ] LITERAL ;
+: chY [ char Y ] LITERAL ;
+: chZ [ char Z ] LITERAL ;
+: ch0 [ char 0 ] LITERAL ;
+: ch1 [ char 1 ] LITERAL ;
+: ch2 [ char 2 ] LITERAL ;
+: ch3 [ char 3 ] LITERAL ;
+: ch4 [ char 4 ] LITERAL ;
+: ch5 [ char 5 ] LITERAL ;
+: ch6 [ char 6 ] LITERAL ;
+: ch7 [ char 7 ] LITERAL ;
+: ch8 [ char 8 ] LITERAL ;
+: ch9 [ char 9 ] LITERAL ;
+
 \ : inchar [ CHAR P ] LITERAL ;
-: inchar nop ; \ placeholder
-: incol  nop ; \ placeholder
+\ placeholders:
+: inchar nop ;
+: incol  nop ;
 
 : at80 ( col char -- )
  swap
- \ 'at80'
+ \ 'at80'                of 'at80=(0,col,char)'
  61 outc 74 outc 38 outc 30 outc
- \ 'at80=('
+ \     '=('              of 'at80=(0,col,char)'
  3D outc 28 outc
- \ 'at80=0,'
+ \       '0,'            of 'at80=(0,col,char)'
  30 outc 2C outc
- \ 'at80=(0,col,'
+ \         'col,'        of 'at80=(0,col,char)'
  incol  outc 2C outc
- \ 'at80=(0,col,char'
- inchar outc \ 2C outc
- \ 'at80=(0,col,char)\n'
- 29 outc line_end s_atd1=() ; \ at80=(0,col,char)
+ \             'char'    of 'at80=(0,col,char)'
+ inchar outc
+ \                 ')\n' of 'at80=(0,col,char)'
+ 29 outc line_end s_atd1=() ;
 
 \ sample run
 \ emit 'PQR' on Lumex 96x8 display:
