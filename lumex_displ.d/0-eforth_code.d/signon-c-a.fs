55509856b7e7fea0c4d409e7083ed3d2ab70a341
wa1tnr <wa1tnr@users.noreply.github.com>
2020-12-18 18:03:54 +0000

    Sept 4th - signon-b defined.
    	new file:   signon-c-a.fs
    
    On branch for-release-PUBLISH

---
+++ lumex_displ.d/0-eforth_code.d/signon-c-a.fs
@@ -0,0 +1,6 @@
+: eflogo ( -- ) \ send eForth v7.20 to Lumex 96x8 RGB LED matrix
+clearit 3 delay 20 outc 20 outc
+65 outc 46 outc 6F outc 72 outc 74 outc 68 outc
+20 outc
+76 outc 37 outc 2E outc 32 outc 30 outc ;
+\ 04 Sep 2020, end.
